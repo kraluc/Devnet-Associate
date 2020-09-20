a786728e5dff12a7d40cfdbb009be625781bdc17
Vincent Ricci <vricci@cisco.com>
2020-09-20 13:32:47 -0700

    initial commit

---
+++ lab01.py
@@ -0,0 +1,15 @@
+# import modules
+import sys
+from helper import *
+
+import xml.etree.ElementTree as try:
+
+import xml.dom.minidom as MD
+
+# Main function
+if __name__ == "__main__":
+    #################################
+    #         Procedure 1           #
+    #################################
+    # Add print statement here
+    print("DevNet")
