4288e262c86ea07bb98989f360f3a227fd027288
Vincent Ricci <vricci@cisco.com>
2020-09-20 15:51:45 -0700

    yaml addition

--- lab01.py
+++ lab01.py
@@ -1,10 +1,11 @@
 # import modules
 import sys
 from helper import *
-
+from ruamel import yaml
 import xml.etree.ElementTree as ET
 
 import xml.dom.minidom as MD
+
 
 # Main function
 if __name__ == "__main__":
@@ -13,3 +14,24 @@ if __name__ == "__main__":
     #################################
     # Add print statement here
     print("DevNet")
+    #################################
+    #         Procedure 2           #
+    #################################
+    print('###################')
+    print('####### YAML ######')
+    print('###################')
+
+    # Open the user.yaml file as read only
+    with open('user.yaml', 'r') as stream:
+        # load the stream using safe_load
+        user_yaml = yaml.safe_load(stream)
+
+    # Print the object type
+    print("ype of user_yaml variable:")
+    print(type(user_yaml))
+    print('----------------------')
+
+    # Iterate over the keys of the user_yaml and print them
+    print('keys in user_yaml:')
+    for key in user_yaml:
+        print(key)
