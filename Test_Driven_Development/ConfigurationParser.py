1f4d05c2f0871703c09d18a8a993cdf4461fd36f
Vincent Ricci <vricci@cisco.com>
2020-11-08 17:46:14 -0800

    added parseCustomerVlan

--- Test_Driven_Development/ConfigurationParser.py
+++ Test_Driven_Development/ConfigurationParser.py
@@ -2,7 +2,6 @@ import re
 import os
 
 FILE = os.path.join(os.path.abspath(os.path.curdir),'Test_Driven_Development','config.txt')
-print(FILE)
 
 class ConfigurationParser:
 
@@ -13,3 +12,16 @@ class ConfigurationParser:
         customerNames = re.findall(customerNamePattern, self.deviceConfig)
         print(customerNames)
         return customerNames
+
+    def parseCustomerVlan(self,customerName):
+        intPattern = (
+            r"interface GigabitEthernet0\/0\.([0-9]+)\n  encapsulation " +
+            r"dot1Q ([0-9]+)\n  ip vrf forwarding %s" % (customerName)
+        )
+        print(f'customer vlan pattern {intPattern}')
+        allCustomerSubInterfaces = re.search(intPattern, self.deviceConfig)
+        print(allCustomerSubInterfaces)
+        print(f'Full Match: {allCustomerSubInterfaces.group(0)}')
+        print(f'first match: {allCustomerSubInterfaces.group(1)}')
+        print(f'Second match: {allCustomerSubInterfaces.group(2)}') # should equal the 1st match
+        return int(allCustomerSubInterfaces.group(1)) # return the 1st match
--- Test_Driven_Development/test_parser.py
+++ Test_Driven_Development/test_parser.py
@@ -1,16 +1,20 @@
 import unittest
 import sys, os
-sys.path.append(os.path.abspath('./Test_Driven_Development'))
+sys.path.append(os.path.join(os.path.curdir,'Test_Driven_Development'))
 from ConfigurationParser import *
 
-
 class TestParse(unittest.TestCase):
+    cp = ConfigurationParser()
     def test_parse_customer_name(self):
-        cp = ConfigurationParser()
         expected_names = ['CUSTOMER_A', 'CUSTOMER_B']
-        parsed_names = cp.parseCustomerNames()
+        parsed_names = self.cp.parseCustomerNames()
         self.assertEqual(list, type(parsed_names))
         self.assertEqual(expected_names, parsed_names)
+    def test_parse_customer_vlan(self):
+        customer_name = 'CUSTOMER_A'
+        expected_vlan = 100
+        parsed_vlan = self.cp.parseCustomerVlan(customer_name)
+        self.assertEqual(expected_vlan, parsed_vlan)
 
 if __name__ == "__main__":
     unittest.main()
